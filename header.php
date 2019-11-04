d0886c4dee691bd2ee253b5db093fcfddaae2c12
Lvzbel <rcoto@protonmail.com>
2019-11-03 22:40:35 -0800

    Links are now fully working

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <body>
   <!-- NAVIGATION -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Duane's Studio</a>
+    <a class="navbar-brand" href="<?php echo site_url('/home'); ?>">Duane's Studio</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -21,19 +21,19 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item <?php if(is_page('home')) echo 'active' ?>">
-          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="<?php echo site_url('/'); ?>">Home</a>
         </li>
         <li class="nav-item <?php if(is_page('gallery')) echo 'active' ?>">
-          <a class="nav-link" href="#">Gallery</a>
+          <a class="nav-link" href="<?php echo site_url('/gallery'); ?>">Gallery</a>
         </li>
         <li class="nav-item <?php if(is_page('blog')) echo 'active' ?>">
-          <a class="nav-link" href="#">Blog</a>
+          <a class="nav-link" href="<?php echo site_url('/blog'); ?>">Blog</a>
         </li>
         <li class="nav-item <?php if(is_page('about')) echo 'active' ?>">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="<?php echo site_url('/about'); ?>">About</a>
         </li>
         <li class="nav-item <?php if(is_page('contact')) echo 'active' ?>">
-          <a class="nav-link" href="#">Contact</a>
+          <a class="nav-link" href="<?php echo site_url('/contact'); ?>">Contact</a>
         </li>
       </ul>
     </div>
