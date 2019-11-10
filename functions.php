f9a1120b917f07c6c8298cfa73e7ddb13896283d
Lvzbel <rcoto@protonmail.com>
2019-11-09 21:08:15 -0800

    Feat: added custom styles and js files

--- functions.php
+++ functions.php
@@ -1,18 +1,33 @@
 <?php
 
-function load_css()
+// Load vendor packages
+function load_bootstrap()
 {
   // Load Bootstrap
   wp_register_style( 'bootstrap', get_template_directory_uri() . '/vendor/css/bootstrap.min.css', array(), false, 'all' );
   wp_enqueue_style( 'bootstrap');
 }
-add_action('wp_enqueue_scripts', 'load_css');
+add_action('wp_enqueue_scripts', 'load_bootstrap');
 
-function load_js()
+function load_jquery()
 {
   wp_enqueue_script( 'jquery');
   // Load Bootstrap.js
   wp_register_script( 'bootstrap', get_template_directory_uri() . '/vendor/js/bootstrap.min.js', 'jquery', false, true );
   wp_enqueue_script( 'bootstrap');
 }
-add_action('wp_enqueue_scripts', 'load_js');
+add_action('wp_enqueue_scripts', 'load_jquery');
+
+// Load custom packages
+function load_styles() {
+  wp_register_style( 'styles', get_template_directory_uri() . '/dist/styles.css', array(), null, 'styles' );
+  wp_enqueue_style('styles');
+}
+add_action('wp_enqueue_scripts', 'load_styles');
+
+function load_scripts() 
+{
+  wp_enqueue_script( 'scrips', get_template_directory_uri() . '/dist/all.js', array(), null, true );
+}
+
+add_action('wp_enqueue_scripts', 'load_scripts');
