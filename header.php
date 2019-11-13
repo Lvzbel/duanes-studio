b5ef53936798cbf283f7d431a0e7e2628a481ac6
Lvzbel <rcoto@protonmail.com>
2019-11-12 21:04:00 -0800

    Feat: navbar is now complete

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 
 <body>
   <!-- NAVIGATION -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-custom">
     <a class="navbar-brand" href="<?php echo site_url('/home'); ?>">Duane's Studio</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
