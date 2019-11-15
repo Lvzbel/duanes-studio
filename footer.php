687e7ff16fbcb3a994f688712bdc41fc5731da62
Lvzbel <rcoto@protonmail.com>
2019-11-14 22:16:23 -0800

    HTML: basic footer markup

--- footer.php
+++ footer.php
@@ -1,3 +1,18 @@
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <p>Contact Us</p>
+        <p>Phone: (xxx) xxx-xxx</p>
+        <p>Email: email@email.com</p>
+      </div>
+      <div class="col-md-6">
+        <p>Social media goes here</p>
+        <p>Each social media will have an icon</p>
+      </div>
+    </div>
+  </div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 
