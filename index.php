d64abe9d234f2e367e7a3e3c57343869b1d3bdf5
Lvzbel <rcoto@protonmail.com>
2019-12-02 21:56:36 -0800

    Feat: index has been made blogs home

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div class="blog__banner jumbotron"
+  style="background-image: url(<?php echo get_theme_file_uri('/src/images/banner-blog.jpg'); ?>);">
+  <h1 class="display-4 font-weight-normal text-light">Blog</h1>
+  <p class="lead text-light">Stay up to the day about my past projects and other announcements</p>
+</div>
+
+<?php get_footer(); ?>
