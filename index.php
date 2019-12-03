8ad3b229131b1f2bab9b7ea33e1a3be2fe6afb90
Lvzbel <rcoto@protonmail.com>
2019-12-02 21:58:11 -0800

    Styles: added margin to blogs header to compansate for navbar

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div class="blog__banner jumbotron"
   style="background-image: url(<?php echo get_theme_file_uri('/src/images/banner-blog.jpg'); ?>);">
-  <h1 class="display-4 font-weight-normal text-light">Blog</h1>
+  <h1 class="blog__header display-4 font-weight-normal text-light">Blog</h1>
   <p class="lead text-light">Stay up to the day about my past projects and other announcements</p>
 </div>
 
