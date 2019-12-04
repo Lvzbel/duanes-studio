013bcbbbffeaf3ac296342278f532215163b221e
Lvzbel <rcoto@protonmail.com>
2019-12-03 21:20:29 -0800

    Feat: fontawesome added

--- header.php
+++ header.php
@@ -8,6 +8,8 @@
   <title><?php bloginfo('name'); ?></title>
   <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 
 <body>
