9f1240ed7b2db65fe761b9724883e4355ba32287
Lvzbel <rcoto@protonmail.com>
2019-12-05 22:33:09 -0800

    Feat: added basic banner

--- page-about.php
+++ page-about.php
@@ -1,8 +1,13 @@
 <?php get_header(); ?>
 
-<div class="container">
-  <h1><?php the_title(); ?></h1>
+<div class="Banner jumbotron">
+  <div class="container">
+    <h1 class="Banner__header display-4 font-weight-normal text-light"><?php the_title(); ?></h1>
+    <p class="lead text-light">Learn more about the man behind the camera.</p>
+  </div>
+</div>
 
+<div class="container">
   <?php get_template_part('includes/section', 'content'); ?>
 </div>
 
