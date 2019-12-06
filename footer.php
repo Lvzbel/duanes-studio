508ab7695880816e54be49faf085dea1aa348d39
Lvzbel <rcoto@protonmail.com>
2019-12-05 21:50:06 -0800

    Style: arrange social media link in footer

--- footer.php
+++ footer.php
@@ -2,17 +2,25 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-        <p>Contact Us</p>
-        <p>Phone: (xxx) xxx-xxx</p>
-        <p>Email: email@email.com</p>
+        <div class="footer__body">
+          <div class="footer__content">
+            <p>Contact Us</p>
+            <p>Phone: (xxx) xxx-xxx</p>
+            <p>Email: email@email.com</p>
+          </div>
+        </div>
       </div>
       <div class="col-md-6">
-        <p>Check out my Social Media!</p>
-        <ul class="Social">
-          <li><a href="#" class="Social__link"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-          <li><a href="#" class="Social__link"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-          <li><a href="#" class="Social__link"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-        </ul>
+        <div class="footer__body">
+          <div class="footer__content">
+            <p>Check out my Social Media!</p>
+            <ul class="Social">
+              <li><a href="#" class="Social__link"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="#" class="Social__link"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a href="#" class="Social__link"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
