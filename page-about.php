6544a5e8e65bfdabfd894818aa81c8e4744bf41c
Lvzbel <rcoto@protonmail.com>
2020-01-09 20:32:50 -0800

    Feat: ACF has been implemented in about header

--- page-about.php
+++ page-about.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-<div class="Banner jumbotron">
+<div style="background-image: url('<?= get_field('header_image')['url'] ?>')" class="Banner jumbotron About">
   <div class="container">
-    <h1 class="Banner__header display-4 font-weight-normal text-light"><?php the_title(); ?></h1>
-    <p class="lead text-light">Learn more about the man behind the camera.</p>
+    <h1 class="Banner__header display-4 font-weight-normal text-light"><?php the_field('header_title'); ?></h1>
+    <p class="lead text-light"><?php the_field('header_description'); ?></p>
   </div>
 </div>
 
-<div class="About container">
+<div class="About__container container">
   <?php get_template_part('includes/section', 'content'); ?>
 </div>
 
