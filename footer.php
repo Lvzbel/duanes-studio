2a298d8cac0263b8330fcdad1ca15a326f5d5b57
Lvzbel <rcoto@protonmail.com>
2020-01-19 00:09:38 -0800

    Feat: footer has now custom contact_info field

--- footer.php
+++ footer.php
@@ -1,12 +1,22 @@
 <footer class="footer text-white">
   <div class="container">
     <div class="row">
+      <?php 
+          $args = array(  
+            'post_type' => 'contact_info',
+            'posts_per_page' => 1
+        );
+    
+        $loop = new WP_Query( $args ); 
+            
+        while ( $loop->have_posts() ) : $loop->the_post(); ?>
       <div class="col-md-6">
         <div class="footer__body">
           <div class="footer__content">
             <p>Contact Us</p>
-            <p>Phone: (xxx) xxx-xxx</p>
-            <p>Email: email@email.com</p>
+            <p>Phone: <?php the_field('phone'); ?></p>
+            <p>Email: <?php the_field('email'); ?></p>
+            <a class='btn btn-secondary btn-sm' href="<?= site_url('/contact'); ?>">Send us a message</a>
           </div>
         </div>
       </div>
@@ -15,13 +25,20 @@
           <div class="footer__content">
             <p>Check out my Social Media!</p>
             <ul class="Social">
-              <li><a href="#" class="Social__link"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="Social__link"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="Social__link"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+              <li><a target="_blank" href="<?php the_field('facebook'); ?>" class="Social__link"><i
+                    class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a target="_blank" href="<?php the_field('instagram'); ?>" class="Social__link"><i
+                    class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a target="_blank" href="<?php the_field('twitter'); ?>" class="Social__link"><i class="fa fa-twitter"
+                    aria-hidden="true"></i></a></li>
             </ul>
           </div>
         </div>
       </div>
+      <?php endwhile;
+    
+        wp_reset_postdata(); 
+      ?>
     </div>
   </div>
 </footer>
